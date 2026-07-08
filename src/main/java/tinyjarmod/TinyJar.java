fe9eacab210d2824023040bc5005cd0390b6ece8
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2026-07-08 22:57:53 +0200

    04: Surface placement. It took just too much time.

--- src/main/java/tinyjarmod/TinyJar.java
+++ src/main/java/tinyjarmod/TinyJar.java
@@ -1,7 +1,9 @@
 package tinyjarmod;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -11,7 +13,7 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.*;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -19,31 +21,84 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
+import org.antlr.v4.runtime.DiagnosticErrorListener;
 
 import java.util.Collections;
 import java.util.List;
 
 public class TinyJar extends Block implements EntityBlock {
-    private static final VoxelShape SHAPE =
-            net.minecraft.world.phys.shapes.Shapes.box(
-                    5.0/16, 0.0, 5.0/16,
-                    11.0/16,8.0/16,11.0/16
-            );
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final EnumProperty<AttachFace> FACE = BlockStateProperties.ATTACH_FACE;
+
     public static final IntegerProperty LIGHT_LEVEL = IntegerProperty.create("light", 0, 15);
 
     public TinyJar(Properties properties) {
         super(properties);
-        registerDefaultState(this.getStateDefinition().any().setValue(LIGHT_LEVEL, 0));
+        registerDefaultState(this.getStateDefinition().any()
+                .setValue(LIGHT_LEVEL, 0)
+                .setValue(FACING, Direction.NORTH)
+                .setValue(FACE, AttachFace.FLOOR)
+        );
     }
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction clickedFace = context.getClickedFace();
 
+        if (clickedFace == Direction.UP) {
+            return defaultBlockState()
+                    .setValue(FACE, AttachFace.FLOOR)
+                    .setValue(FACING, context.getHorizontalDirection().getOpposite());
+        }
+
+        if (clickedFace == Direction.DOWN) {
+            return defaultBlockState()
+                    .setValue(FACE, AttachFace.CEILING)
+                    .setValue(FACING, context.getHorizontalDirection());
+        }
+
+        return defaultBlockState()
+                .setValue(FACE, AttachFace.WALL)
+                .setValue(FACING, clickedFace.getOpposite());
+    }
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder){
-        builder.add(LIGHT_LEVEL);
+        builder.add(LIGHT_LEVEL, FACING, FACE);
     }
 
     @Override
     public VoxelShape getShape(BlockState st, BlockGetter lv, BlockPos pos, CollisionContext cont){
-        return SHAPE;
+        return switch (st.getValue(FACE)) {
+            case FLOOR -> net.minecraft.world.phys.shapes.Shapes.box(
+                    5.0/16, 0.0, 5.0/16,
+                    11.0/16,8.0/16,11.0/16
+            );
+            case CEILING -> net.minecraft.world.phys.shapes.Shapes.box(
+                    5.0/16, 8.0/16, 5.0/16,
+                    11.0/16,16.0/16,11.0/16
+            );
+            case WALL -> switch (st.getValue(FACING)) {
+                case NORTH -> net.minecraft.world.phys.shapes.Shapes.box(
+                    5.0/16, 5.0/16, 0.0,
+                    11.0/16,11.0/16,8.0/16
+                );
+                case SOUTH -> net.minecraft.world.phys.shapes.Shapes.box(
+                        5.0/16, 5.0/16, 8.0/16,
+                        11.0/16,11.0/16,16.0/16
+                );
+                case EAST  -> net.minecraft.world.phys.shapes.Shapes.box(
+                        8.0/16, 5.0/16, 5.0/16,
+                        16.0/16,11.0/16,11.0/16
+                );
+                case WEST -> net.minecraft.world.phys.shapes.Shapes.box(
+                        0.0, 5.0/16, 5.0/16,
+                        8.0/16,11.0/16,11.0/16
+                );
+                default -> net.minecraft.world.phys.shapes.Shapes.box(
+                        5.0/16, 0.0, 5.0/16,
+                        11.0/16,8.0/16,11.0/16
+                );
+            };
+        };
     }
 
     @Override
--- src/main/java/tinyjarmod/client/render/TinyJarRenderer.java
+++ src/main/java/tinyjarmod/client/render/TinyJarRenderer.java
@@ -2,16 +2,23 @@ package tinyjarmod.client.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
 import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidStack;
 import tinyjarmod.TinyJarBlockEntity;
+
+import static tinyjarmod.TinyJar.FACE;
+import static tinyjarmod.TinyJar.FACING;
 
 public class TinyJarRenderer implements BlockEntityRenderer<TinyJarBlockEntity> {
 
@@ -20,6 +27,24 @@ public class TinyJarRenderer implements BlockEntityRenderer<TinyJarBlockEntity> 
     @Override
     public void render(TinyJarBlockEntity be, float partialTick, PoseStack poseStack,
                        MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
+        BlockState state = be.getBlockState();
+        AttachFace face = state.getValue(FACE);
+        Direction facing = state.getValue(FACING);
+
+        poseStack.translate(0.5, 0.5, 0.5);
+        poseStack.mulPose(switch (face){
+                    case FLOOR -> Axis.XP.rotationDegrees(0);
+                    case CEILING -> Axis.XP.rotationDegrees(180);
+                    case WALL -> switch (facing){
+                        case NORTH -> Axis.XP.rotationDegrees(90);
+                        case SOUTH -> Axis.XP.rotationDegrees(-90);
+                        case EAST -> Axis.ZP.rotationDegrees(90);
+                        case WEST -> Axis.ZP.rotationDegrees(-90);
+                        default -> Axis.XP.rotationDegrees(0);
+                    };
+                }
+        );
+        poseStack.translate(-0.5, -0.5, -0.5);
 
         FluidStack fluidStack = be.getTank().getFluid();
         if (fluidStack.isEmpty()) return;
